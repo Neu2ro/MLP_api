8bd2d231f0a73cb65f2cc13f218047925cc88d86
Vivek Balani <90723947+Neu2ro@users.noreply.github.com>
2023-03-09 09:27:00 +0530

    requirements updated

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def extract_feature(file_name, mfcc, chroma, mel):
 #     return jsonify({'Emotion': predicted_emotion})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='8000')
